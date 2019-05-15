8df13294fb4d75da3284e578379083430898bf19
Alexander Fountain <alexander@digett.com>
2019-05-14 20:57:10 -0500

    doctor folder

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -153,7 +153,7 @@ export const pageQuery = graphql`
         slug
       }
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date
         title
         tags
         image{
