5f3b085dc236556529a2eb77e154976f0b3bdeee
Alexander Fountain <alexander@digett.com>
2019-06-04 17:51:40 -0500

    dontenv

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,5 @@
 const queries = require(`${__dirname}/src/templates/algolia`)
 
-let activeEnv =
-  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
-
-console.log(`Using environment config: '${activeEnv}'`)
-
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
